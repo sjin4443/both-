f5bfa332dace0746fab830f37e27a4ddae60a356
sjin4443 <71583444+sjin4443@users.noreply.github.com>
2020-10-28 14:21:50 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+    $(".ticket1_front").click(function(){
+        $(".ticket1_back").css({
+        'display':'inline-block'
+            })
+    })
+
+    $(".ticket2_front").click(function(){
+        $(".ticket2_back").css({
+        'display':'inline-block'
+            })
+    })
+
+})
